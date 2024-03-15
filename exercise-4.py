de49d62eb39bc4293c2e7b5fe8b8ae29b7fdf7a4
soklinkim <156177371+soklinkim@users.noreply.github.com>
2024-03-15 11:32:59 +0700

    done but still at invalid value

--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,17 @@
 def chunking_by(numbers, chunck):
-    ...
+    
+    # create a list to store the result
+    chuck_result = []
+    
+    # loop number in range from 0 to allnumbers with step
+    for index in range(0, len(numbers), chunck):
+        
+        # in numbers take first index + step
+        chuck = numbers[index:index + chunck]  
+        chuck_result.append(chuck)
+        
+    return chuck_result
+
+
+            
+        
