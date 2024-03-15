a8183c09fc7889308ba3238bae740e988461af3c
sophanithsom <2023468som@aupp.edu.kh>
2024-03-15 21:15:56 +0700

    first solution

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,17 @@
 def remove_all_after(numbers, n):
-    print("hi")
+    result = []
+    #check if the list is empty or the n element cant be found, return the list back
+    if len(numbers) == 0 or n not in numbers:
+        return numbers
+    
+    for num in numbers:
+        if num != n:
+            result.append(num)
+        else:
+            #if the cutting element is found, add it to the result and break the loop
+            result.append(num)
+            break
+                
+    return result
+# print(remove_all_after([1, 2, 3, 4, 5], 3))
+
