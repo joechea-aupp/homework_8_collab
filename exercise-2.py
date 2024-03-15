4a3b566be3beb019f9ce73e6abd1b8550cbbcb82
Boreyvina <2024053kim@aupp.edu.kh>
2024-03-15 14:07:54 +0700

    Done ex2

--- exercise-2.py
+++ exercise-2.py
@@ -1,4 +1,19 @@
 def index_power(numnbers, n):
     ...
-    #for number in numbers:
-    print("thank you")
+    result = 0
+    #condition if n is outside array of numnbers
+    if n >= len(numnbers):
+            result -= 1
+    
+    if n in numnbers:
+        #caculate if n is inside array of numnbers
+        result = numnbers[n] ** n 
+
+    return result
+
+print( index_power([1, 2, 3, 4], 2) )
+print(index_power([1, 3, 10, 100], 3)) 
+print( index_power([0, 1], 0) )
+print( index_power([1, 2], 3) )
+
+
