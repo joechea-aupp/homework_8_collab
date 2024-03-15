4efe4bc1c125a0a39e9396a6091c27dec7c16363
soklinkim <156177371+soklinkim@users.noreply.github.com>
2024-03-15 13:54:31 +0700

    Done

--- exercise-4.py
+++ exercise-4.py
@@ -1,5 +1,10 @@
 def chunking_by(numbers, chunck):
     
+    if len(number) == 0:
+        print("The list is empty!")
+        
+        
+
     # create a list to store the result
     chuck_result = []
     
@@ -11,6 +16,12 @@ def chunking_by(numbers, chunck):
         chuck_result.append(chuck)
         
     return chuck_result
+
+# for checking
+number = [1, 2, 3, 4, 5]
+chunk_size = 3
+
+print(chunking_by(number, chunk_size))
 
 
             
