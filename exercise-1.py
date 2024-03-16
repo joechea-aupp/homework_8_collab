06fe787ca31d45b72d8622457e0ea4357bb260ca
SereyratanakHeng <2023472heng@aupp.edu.kh>
2024-03-17 02:09:39 +0700

    exercise1.solut

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,14 @@
+# numbers = [1, 2, 3, 4]
+
 def replace_last(numbers):
-    ...
+    # check the length of list or the list is empty
+    if len(numbers) < 2:
+        return numbers
+    
+    else:
+        last_element = numbers[0]
+        numbers[0] = numbers[-1]
+        numbers[-1] = last_element
+        return numbers
+    
+# print(replace_last(numbers))
